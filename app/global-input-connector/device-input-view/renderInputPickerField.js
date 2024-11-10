2aeb8bf5bce67c78495e60f30eab178b7b5f02ef
Dilshat Hewzulla <hewzulla@gmail.com>
2024-11-10 12:50:28 +0000

    input-picker

--- app/global-input-connector/device-input-view/renderInputPickerField.js
+++ app/global-input-connector/device-input-view/renderInputPickerField.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import {View} from 'react-native';
 
-import {styles} from "../styles";
-
-
+import InputPicker from '../../components/input/InputPicker';
+import {styles} from '../styles';
 export default ({dataitem, onFieldChanged}) => {
-    var selectedItem=null;
-    if(dataitem.items){
-      for(var i=0;i<dataitem.items.length;i++){
-        if(dataitem.items[i].value===dataitem.value){
-          selectedItem=dataitem.items[i];
-        }
+  var selectedItem = null;
+  if (dataitem.items) {
+    for (var i = 0; i < dataitem.items.length; i++) {
+      if (dataitem.items[i].value === dataitem.value) {
+        selectedItem = dataitem.items[i];
       }
-    }    
-    return(
-      <View style={styles.inputContainer}>
-          <InputPicker
-            selectedItem={selectedItem}
-            items={dataitem.items}
-            label={dataitem.label}
-            onValueChange={value=>{
-                onFieldChanged(value);
-            }}/>
-        </View>
-    );
-  };
+    }
+  }
+  return (
+    <View style={styles.inputContainer}>
+      <InputPicker
+        selectedItem={selectedItem}
+        items={dataitem.items}
+        label={dataitem.label}
+        onValueChange={value => {
+          onFieldChanged(value);
+        }}
+      />
+    </View>
+  );
+};
