25123fc3eb3c144c2d98255b4124f9e33efb4ae1
Dilshat Hewzulla <hewzulla@gmail.com>
2025-01-04 19:22:09 +0000

    test

--- app/store/configStore.js
+++ app/store/configStore.js
@@ -1,30 +1,29 @@
-import AsyncStorage from "@react-native-community/async-storage";
+import AsyncStorage from '@react-native-community/async-storage';
 
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
-import thunk from "redux-thunk";
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import thunk from 'redux-thunk';
 
-import { globalInputSettings } from "./reducers/globalInputSettings";
-import { userSettings, generateRandomString } from "./reducers/userSettings";
-import { domainFormMappings } from "./reducers/domainFormMappings";
+import {globalInputSettings} from './reducers/globalInputSettings';
+import {userSettings, generateRandomString} from './reducers/userSettings';
+import {domainFormMappings} from './reducers/domainFormMappings';
 
-import { persistStore, persistReducer } from "redux-persist"
+import {persistStore, persistReducer} from 'redux-persist';
 var reducers = combineReducers({
-    globalInputSettings: globalInputSettings.reducer,
-    userSettings: userSettings.reducer,
-    domainFormMappings: domainFormMappings.reducer
+  globalInputSettings: globalInputSettings.reducer,
+  userSettings: userSettings.reducer,
+  domainFormMappings: domainFormMappings.reducer,
 });
 
 const appliedMiddleWare = applyMiddleware(thunk);
 
 const persistConfig = {
-    key: 'root',
-    storage: AsyncStorage
+  key: 'root',
+  storage: AsyncStorage,
 };
 const persistedReducer = persistReducer(persistConfig, reducers);
 export default function configStore(onCompletion) {
-
-    const store = createStore(persistedReducer, appliedMiddleWare);
-    persistor = persistStore(store, null, onCompletion);
-    return { store, persistor };
+  const store = createStore(persistedReducer, appliedMiddleWare);
+  persistor = persistStore(store, null, onCompletion);
+  return {store, persistor};
 }
-export { generateRandomString };
+export {generateRandomString};
